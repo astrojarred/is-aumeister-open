02a4fe9b876524b088114a593fb16d1d41734efa
Jarred Green <astrojarred@gmail.com>
2023-04-04 13:41:41 +0200

    first commit

---
+++ src/routes/api/open/+server.ts
@@ -0,0 +1,27 @@
+import * as cheerio from 'cheerio';
+
+import { error, json } from '@sveltejs/kit';
+
+import fetch from 'node-fetch';
+
+const URL = "https://www.aumeister.de/willkommen/"
+//const URL = "https://web.archive.org/web/20220714005455/https://www.aumeister.de/willkommen/"
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET() {
+  try {
+    const response = await fetch(URL);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    const targetElement = $('div#c769 p.text-center');
+    const extractedText = targetElement.text();
+
+    const isOpen = extractedText.includes("geöffnet")
+
+
+    return json({message: extractedText, open: isOpen});
+  } catch (error) {
+    console.error('Error fetching and parsing HTML:', error);
+    throw error(500, 'Error fetching from the aumeister website.')
+  }
+}
