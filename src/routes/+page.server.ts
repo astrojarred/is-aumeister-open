040a5f52eb3cc2c87fd622f0db2f5a94a3909b5f
Jarred Green <astrojarred@gmail.com>
2023-04-04 14:07:27 +0200

    Add site functionality

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,32 @@
+import * as cheerio from 'cheerio';
+
+import { error } from '@sveltejs/kit';
+import fetch from 'node-fetch';
+
+const URL = 'https://www.aumeister.de/willkommen/';
+
+/** @type {import('./$types').PageLoad} */
+export async function load() {
+	try {
+        let isOpen;
+		const response = await fetch(URL);
+		const html = await response.text();
+		const $ = cheerio.load(html);
+		const targetElement = $('div#c769 p.text-center');
+		const extractedText = targetElement.text();
+
+		if (extractedText) {
+			isOpen = extractedText.includes('geöffnet');
+		}
+        else {
+            throw error(500, 'Error fetching from the aumeister website.')
+        }
+
+		return { message: extractedText, open: isOpen };
+	} catch (e) {
+		console.log('ERROR');
+		console.log(e);
+		// raise error
+		throw { status: 500, error: new Error('There was an issue.') };
+	}
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	export let data;
+	console.log(data.message);
+
+	let showDetails = false;
+</script>
+
+{#if data}
+	<div class="h-screen flex flex-col items-center justify-center">
+		<div class="cursor-pointer"
+			on:click={() => {
+				showDetails = !showDetails;
+			}}
+			on:keypress={() => {
+				showDetails = !showDetails;
+			}}
+		>
+			<h1 class="text-7xl">
+				{data?.open ? 'YES.' : 'NO.'}
+			</h1>
+		</div>
+        {#if showDetails}
+        <p class="text-lg mx-5 lg:max-w-lg">{data.message}</p>
+        {/if}
+	</div>
+{:else}
+	<div class="h-screen flex items-center justify-center">
+		<h1 class="text-7xl">Loading...</h1>
+	</div>
+{/if}
