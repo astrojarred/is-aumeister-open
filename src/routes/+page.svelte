2f2e7190d25702c80ba1119fae09fde1a4d0833a
Jarred Green <astrojarred@gmail.com>
2023-04-04 15:09:31 +0200

    Reformat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,28 @@
 <script>
-	import { update_await_block_branch } from 'svelte/internal';
-
-
 	export let data;
 	console.log(data.message);
 
-    const germanyTimeZone = 'Europe/Berlin';
-    const now = new Date();
-    const germanyTime = new Date(now.toLocaleString('en-US', { timeZone: germanyTimeZone }));
+	const germanyTimeZone = 'Europe/Berlin';
+	const now = new Date();
+	const germanyTime = new Date(now.toLocaleString('en-US', { timeZone: germanyTimeZone }));
 
-    // Calculate the difference between local time and Germany time in milliseconds
-    const offset = germanyTime.getTime() - now.getTime();
-    
-    // Add the offset to the current local time to get the Germany time as a Date object
-    const germanyDate = new Date(now.getTime() + offset);
+	// Calculate the difference between local time and Germany time in milliseconds
+	const offset = germanyTime.getTime() - now.getTime();
 
-    const updatingSoon = germanyDate.getHours() < 11;
+	// Add the offset to the current local time to get the Germany time as a Date object
+	const germanyDate = new Date(now.getTime() + offset);
 
-    console.log('Current time in Germany:', germanyDate.getTime(), updatingSoon);
+	const updatingSoon = germanyDate.getHours() < 11;
+
+	console.log('Current time in Germany:', germanyDate.getTime(), updatingSoon);
 
 	let showDetails = false;
 </script>
 
 {#if data}
 	<div class="h-screen flex flex-col items-center justify-center">
-		<div class="cursor-pointer"
+		<div
+			class="cursor-pointer"
 			on:click={() => {
 				showDetails = !showDetails;
 			}}
@@ -36,12 +34,14 @@
 				{data?.open ? '🍻YES.' : 'NO.'}
 			</h1>
 		</div>
-        {#if updatingSoon}
-            <p class="text-lg mx-5 lg:max-w-lg text-red-300">Attention: This could change around 10 or 11AM!</p>
-        {/if}
-        {#if showDetails}
-        <p class="text-lg mx-5 lg:max-w-lg">{data.message}</p>
-        {/if}
+		{#if updatingSoon}
+			<p class="text-lg mx-5 lg:max-w-lg text-red-300">
+				Attention: This could change around 10 or 11AM!
+			</p>
+		{/if}
+		{#if showDetails}
+			<p class="text-lg mx-5 lg:max-w-lg">{data.message}</p>
+		{/if}
 	</div>
 {:else}
 	<div class="h-screen flex items-center justify-center">
